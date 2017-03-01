0619dc0e02f95218882bb7ae393620a0ff62fb8e
kparke-ncc <kristin.parke@nccgroup.trust>
2017-03-01 12:05:47 -0800

    Switched db from sqlite3 to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170227223922) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "responses", force: :cascade do |t|
     t.text     "text"
     t.datetime "created_at", null: false
